018458e7860766eca6ede37484b377d5f63ebc7b
Yaroslav2777 <y.negoda@istu.edu.ua>
2026-03-24 20:57:23 +0200

    .

---
+++ Task1/Task1/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Task1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPassword1 = new System.Windows.Forms.TextBox();
+            this.numDay = new System.Windows.Forms.NumericUpDown();
+            this.numMonth = new System.Windows.Forms.NumericUpDown();
+            this.numYear = new System.Windows.Forms.NumericUpDown();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lDiagnost = new System.Windows.Forms.Label();
+            this.fgff = new System.Windows.Forms.Label();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Прізвище";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ім\'я";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Дата народження";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Стать";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Пароль";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(76, 248);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Повторний пароль";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(193, 36);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(100, 20);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(193, 75);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(193, 172);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtLogin.TabIndex = 11;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(193, 208);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 12;
+            // 
+            // txtPassword1
+            // 
+            this.txtPassword1.Location = new System.Drawing.Point(193, 245);
+            this.txtPassword1.Name = "txtPassword1";
+            this.txtPassword1.PasswordChar = '*';
+            this.txtPassword1.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword1.TabIndex = 13;
+            // 
+            // numDay
+            // 
+            this.numDay.Location = new System.Drawing.Point(193, 109);
+            this.numDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numDay.Name = "numDay";
+            this.numDay.Size = new System.Drawing.Size(45, 20);
+            this.numDay.TabIndex = 14;
+            this.numDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numMonth
+            // 
+            this.numMonth.Location = new System.Drawing.Point(248, 109);
+            this.numMonth.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numMonth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numMonth.Name = "numMonth";
+            this.numMonth.Size = new System.Drawing.Size(45, 20);
+            this.numMonth.TabIndex = 15;
+            this.numMonth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numYear
+            // 
+            this.numYear.Location = new System.Drawing.Point(299, 109);
+            this.numYear.Maximum = new decimal(new int[] {
+            2026,
+            0,
+            0,
+            0});
+            this.numYear.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.numYear.Name = "numYear";
+            this.numYear.Size = new System.Drawing.Size(45, 20);
+            this.numYear.TabIndex = 16;
+            this.numYear.Value = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(429, 43);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(137, 37);
+            this.btnOK.TabIndex = 20;
+            this.btnOK.Text = "Зберегти";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(429, 92);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(137, 37);
+            this.btnCancel.TabIndex = 21;
+            this.btnCancel.Text = "Вихід";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(410, 189);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(327, 173);
+            this.txtOutput.TabIndex = 22;
+            // 
+            // lDiagnost
+            // 
+            this.lDiagnost.AutoSize = true;
+            this.lDiagnost.Location = new System.Drawing.Point(76, 291);
+            this.lDiagnost.Name = "lDiagnost";
+            this.lDiagnost.Size = new System.Drawing.Size(11, 13);
+            this.lDiagnost.TabIndex = 23;
+            this.lDiagnost.Text = "*";
+            // 
+            // fgff
+            // 
+            this.fgff.AutoSize = true;
+            this.fgff.Location = new System.Drawing.Point(76, 172);
+            this.fgff.Name = "fgff";
+            this.fgff.Size = new System.Drawing.Size(34, 13);
+            this.fgff.TabIndex = 4;
+            this.fgff.Text = "Логін";
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(269, 141);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(61, 17);
+            this.rbFemale.TabIndex = 18;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Жіноча";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(193, 139);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(70, 17);
+            this.rbMale.TabIndex = 17;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Чоловіча";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.lDiagnost);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.numYear);
+            this.Controls.Add(this.numMonth);
+            this.Controls.Add(this.numDay);
+            this.Controls.Add(this.txtPassword1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.fgff);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numDay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numYear)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtPassword1;
+        private System.Windows.Forms.NumericUpDown numDay;
+        private System.Windows.Forms.NumericUpDown numMonth;
+        private System.Windows.Forms.NumericUpDown numYear;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lDiagnost;
+        private System.Windows.Forms.Label fgff;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+    }
+}
+
---
+++ Task1/Task1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+            txtSurname.ForeColor = Color.Gray;
+            txtName.ForeColor = Color.Gray;
+            txtLogin.ForeColor = Color.Gray;
+        }
+        private void TxtSurname_TextChanged(object sender, EventArgs e)
+        {
+            txtSurname.ForeColor = Color.Black;
+            foreach (char c in txtSurname.Text)
+                if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
+                    lDiagnost.Text = "* У прізвищі мають бути лише літери";
+        }
+
+        private void TxtName_TextChanged(object sender, EventArgs e)
+        {
+            txtName.ForeColor = Color.Black;
+            foreach (char c in txtName.Text)
+                if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
+                    lDiagnost.Text = "* В імені мають бути лише літери";
+        }
+
+        private void TxtLogin_TextChanged(object sender, EventArgs e)
+        {
+            txtLogin.ForeColor = Color.Black;
+        }
+
+        private bool CheckEmptyTextBox()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtSurname.Text) || txtSurname.Text == "Прізвище" ||
+                    string.IsNullOrWhiteSpace(txtName.Text) || txtName.Text == "Ім'я" ||
+                    string.IsNullOrWhiteSpace(txtLogin.Text))
+                    throw new Exception("Усі текстові поля обов'язкові.");
+
+                if (string.IsNullOrEmpty(txtPassword.Text) || string.IsNullOrEmpty(txtPassword1.Text))
+                    throw new Exception("Введіть пароль.");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                lDiagnost.Text = "* " + ex.Message;
+                return false;
+            }
+        }
+
+        private bool CheckPassword()
+        {
+            try
+            {
+                if (txtPassword.Text != txtPassword1.Text)
+                    throw new Exception("Паролі не співпадають.");
+                if (txtPassword.Text.Length < 6)
+                    throw new Exception("Пароль має бути не менше 6 символів.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                lDiagnost.Text = "* " + ex.Message;
+                return false;
+            }
+        }
+
+        private bool CheckLogin()
+        {
+            Regex myReg1 = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+
+            if (!myReg1.IsMatch(txtLogin.Text))
+            {
+                lDiagnost.Text = "* Невірний формат логіну (Email).";
+                return false;
+            }
+            return true;
+        }
+
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            lDiagnost.Text = "";
+
+            if (CheckEmptyTextBox() && CheckPassword() && CheckLogin())
+            {
+                string gender = rbMale.Checked ? "Чоловіча" : "Жіноча";
+                string birthDate = $"{numDay.Value:00}.{numMonth.Value:00}.{numYear.Value}";
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("--- РЕЄСТРАЦІЙНІ ДАНІ ---");
+                sb.AppendLine($"Прізвище: {txtSurname.Text}");
+                sb.AppendLine($"Ім'я: {txtName.Text}");
+                sb.AppendLine($"Логін: {txtLogin.Text}");
+                sb.AppendLine($"Стать: {gender}");
+                sb.AppendLine($"Дата народження: {birthDate}");
+                sb.AppendLine($"Пароль: {new string('*', txtPassword.Text.Length)}");
+                sb.AppendLine("-------------------------");
+
+                txtOutput.Text = sb.ToString();
+                lDiagnost.Text = "Дані успішно збережено!";
+                lDiagnost.ForeColor = Color.Green;
+            }
+            else
+            {
+                lDiagnost.ForeColor = Color.Red;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
